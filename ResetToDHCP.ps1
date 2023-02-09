cb380934c1440a4171135fde4cc7d8b8ce2ada24
JDTMH <124918363+JDTMH@users.noreply.github.com>
2023-02-09 13:42:01 -0500

    Uploading some more scripts
    Uploading some more scripts

---
+++ ResetToDHCP.ps1
@@ -0,0 +1,20 @@
+﻿<#  ResetToDHCP.ps1
+
+    A script to reset the interface to dhcp.
+    
+    Date: 12/27/2022 
+#>
+
+$IPType = "IPv4"
+$adapter = Get-NetAdapter | ? {$_.Status -eq "up"}
+$interface = $adapter | Get-NetIPInterface -AddressFamily $IPType
+If ($interface.Dhcp -eq "Disabled") {
+ # Remove existing gateway
+ If (($interface | Get-NetIPConfiguration).Ipv4DefaultGateway) {
+ $interface | Remove-NetRoute -Confirm:$false
+ }
+ # Enable DHCP
+ $interface | Set-NetIPInterface -DHCP Enabled
+ # Configure the DNS Servers automatically
+ $interface | Set-DnsClientServerAddress -ResetServerAddresses
+}
---
+++ SetControlPanelSmallIcons.ps1
@@ -0,0 +1,13 @@
+﻿<#  SetControlPanelSmallIcons.ps1
+
+    A script to rset small icons in the control panel.
+    
+    Date: 12/27/2022 
+#>
+
+Write-Output "Setting Control Panel view to small icons..."
+    If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel")) {
+        New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" | Out-Null
+    }
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" -Name "StartupPage" -Type DWord -Value 1
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" -Name "AllItemsIconView" -Type DWord -Value 1
